e6d20c69dd366a1454ce9e6335ab99ddb82ff1a3
Rob Chadwick <robert.chadwick.ctr@adlnet.gov>
2013-11-12 16:16:39 -0500

    adding first files for side tabs

---
+++ support/client/lib/vwf/view/editorview/SideTabs.js
@@ -0,0 +1,16 @@
+define(function ()
+{
+	function initialize()
+	{
+		$(document.body).append("<div id='sidetabs' />");
+
+		$("#sidetabs").append("<div id='SideTabModels' class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabProperties'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabMaterials'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabHierarchy'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabUsers'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabInventory'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabScripts'  class='sidetab'>Chat</div>");
+		$("#sidetabs").append("<div id='SideTabChat'  class='sidetab'>Chat</div>");
+	}
+});
