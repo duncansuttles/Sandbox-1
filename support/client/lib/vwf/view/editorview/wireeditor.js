34eb7fca8f043773158b8b2e0d9315ac32c4f783
rchadwic <rchadwic@gmail.com>
2013-12-18 09:30:47 -0500

    wire editor working

--- support/client/lib/vwf/view/editorview/wireeditor.js
+++ support/client/lib/vwf/view/editorview/wireeditor.js
@@ -17,18 +17,83 @@ define(function ()
 	function initialize()
 	{
 		$(document.body).append("<div id='WireEditor'></div>");
-		$("#WireEditor").append("<div><div id='WireEditorSelectedName'></div><div id='WireEditorSourceName'></div>");
-		$("#WireEditor").append("<div><div id='WireEditorSelectedProps'></div><div id='WireEditorSourceProps'></div></div>");
+		$("#WireEditor").append("<div class='wireeditorleft'><div id='WireEditorSelectedName'></div><div id='WireEditorSelectedProps'></div></div>");
+		$("#WireEditor").append("<div class='wireeditorright'><div id='WireEditorSourceName'></div><div id='WireEditorSourceProps'></div></div>");
+		$("#WireEditor").append("<div id='wireeditorbuttons' />");
+		$("#wireeditorbuttons").append("<div id='SubExp1'>Expression</div><div id='wireeditorLink' /><div id='SubExp2'>Expression</div>");
 		this.selectedProp = null;
 		this.selectedNodeID = null;
 		this.pickSourceID = null;
 		this.pickSourceProp = null;
 		this.currentWires = null;
+		this.subExp1 = null;
+		this.subExp2 = null;
+		var self = this;
+		
+		
+		$(".wireeditorright").append("<div id='wireeditordisable'/>");
+		$("#wireeditordisable").hide();
+		this.setSubExp1 = function(e)
+		{
+			$('#SubExp1').text("Expression: " + e)
+			this.subExp1 = typeof (e) == 'string' ?e.split('.'):null;
+		}
+		this.setSubExp2 = function(e)
+		{
+			$('#SubExp2').text("Expression: " + e)
+			this.subExp2 = typeof (e) == 'string' ?e.split('.'):null;
+		}
+		$('#SubExp1').click(function(){
+			alertify.prompt("Enter the sub expression as Prop.SubProp...",function(ok,val){
+
+				if(ok)
+				self.setSubExp1(val);
+			})
+
+		});
+		$('#SubExp2').click(function(){
+			alertify.prompt("Enter the sub expression as Prop.SubProp...",function(ok,val){
+
+				if(ok)
+				self.setSubExp2(val);
+			})
+
+		});
+		this.linkOrUnlink = function()
+		{
+			if(self.hasWire())
+			{
+				var i = 0;
+				for(i =0; i < self.currentWires.length; i++)
+				{
+					var wire = self.currentWires[i];
+					if(wire[1]==self.selectedProp)
+					{
+						break;
+					}
+				}
+				self.currentWires.splice(i,1);
+				vwf_view.kernel.setProperty(self.selectedNodeID,'wires',self.currentWires);
+			}
+			else
+			{
+				self.currentWires.push([self.pickSourceID,self.selectedProp,self.pickSourceProp,self.subExp1,self.subExp2,null])
+				vwf_view.kernel.setProperty(self.selectedNodeID,'wires',self.currentWires);
+
+			}	
+			self.selectProp(self.selectedProp);
+
+		}
+		$('#wireeditorLink').click(this.linkOrUnlink);
+		$('#wireeditorLink').hide();
+		$('#SubExp1').hide();
+		$('#SubExp2').hide();
 		$('#WireEditor').dialog(
 		{
 			modal: true,
 			autoOpen: false,
-			resizable: true
+			resizable: true,
+			title:"Wire Editor"
 		});
 		this.Show = function()
 		{
@@ -61,37 +126,127 @@ define(function ()
 			}
 			return properties;
 		}
-		this.setSource = function(e)
+		this.pickSource = function(e)
 		{
-
-
+			$('#WireEditor').dialog('close')
+			_Editor.SetSelectMode('TempPick');
+			$('#wireeditorLink').hide();
+			$('#SubExp1').hide();
+				$('#SubExp2').hide();
+			_Editor.TempPickCallback = function(e)
+			{
+				$('#WireEditor').dialog('open')
+				_Editor.SetSelectMode('None');
+				_Editor.SelectObject();
+				self.setSource(e);
+			}
 		}
+		this.pickTarget = function(e)
+		{
+			$('#WireEditor').dialog('close')
+			_Editor.SetSelectMode('TempPick');
+			$('#wireeditorLink').hide();
+			$('#SubExp1').hide();
+				$('#SubExp2').hide();
+			_Editor.TempPickCallback = function(e)
+			{
+				$('#WireEditor').dialog('open')
+				_Editor.SetSelectMode('None');
+				_Editor.SelectObject();
+				self.selectedNodeID = e.id;
+				self.currentWires = vwf.getProperty(self.selectedNodeID,'wires');
+				self.buildGUI();
+				self.setSource(self.pickSourceID);
+			}
+		}
+		$('#WireEditorSourceName').click(this.pickSource);
+		$('#WireEditorSelectedName').click(this.pickTarget);
 		this.setSource = function(e)
 		{
-			this.pickSourceID = e;
+			if(!e)
+			{
+				this.pickSourceID = e;
+				$("#wireeditordisable").hide();
+				$(".wireeditorright").removeClass('wiredisabled');
+				return;
+			}
+			this.pickSourceID = e.id || e;
+			
+
+			$('#WireEditorSourceName').text(vwf.getProperty(this.pickSourceID,'DisplayName') ||this.pickSourceID);
 			$('#WireEditorSourceProps').empty();
 			var props = this.getProperties(this.pickSourceID);
 			for(var i in props)
 			{
-				$('#WireEditorSourceProps').append("<div class='sourcepropchoice' id='targetprop"+i+"'>"+i+"</div>");
+				$('#WireEditorSourceProps').append("<div class='sourcepropchoice' id='sourceprop"+i+"'>"+i+"</div>");
 				var self = this;
 
-				$('#targetprop'+i).click(function(e)
+				$('#sourceprop'+i).click(function(e)
 				{
-					self.selectProp($(this).text());
-					$(".sourcepropchoice").removeClass('wirepropselected');
-					$(this).addClass('wirepropselected');
+					self.setSourceProp($(this).text());
+					
 				})
 			}
 		}
+		this.updateTargetProps = function()
+		{
+				$(".targetpropchoice").removeClass('wirepropwired');
+				for(i =0; i < self.currentWires.length; i++)
+				{
+					var wire = self.currentWires[i];
+					var e = wire[1];
+					$(".targetpropchoice").filter(":contains("+e+")").addClass('wirepropwired');
+				}
+
+		}
+		this.hasWire = function()
+		{
+			for(var i =0; i < this.currentWires.length; i++)
+			{
+				var wire = this.currentWires[i];
+				if(wire[1]==this.selectedProp)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		this.setSourceProp = function(e)
+		{
+			self.pickSourceProp = e;
+			$('#wireeditorLink').css( "display", "inline-block");
+			if(!self.hasWire())
+			{
+				$('#wireeditorLink').text('Create Wire');
+				$('#SubExp1').css( "display", "inline-block");
+				$('#SubExp2').css( "display", "inline-block");
+			}
+			else
+			{
+				$('#wireeditorLink').text('Break Wire');
+			}
+
+			$(".sourcepropchoice").removeClass('wirepropselected');
+			$(".sourcepropchoice").filter(":contains("+e+")").addClass('wirepropselected');
+			self.setSubExp2(null);
+		}
 		this.selectProp = function(e)
 		{
+			this.updateTargetProps();
 			this.selectedProp = e;
 			var wiredToID = null;
 			var wiredToProp = null;
 			var wireSubTarget = null;
 			var wireSubSource = null;
 			var wireFunc = null;
+			$('#wireeditorLink').hide();
+			$('#SubExp1').hide();
+			$('#SubExp2').hide();
+			$(".sourcepropchoice").removeClass('wirepropselected');
+			self.pickSourceProp = null;
+			$("#wireeditordisable").hide();
+			$(".wireeditorright").removeClass('wiredisabled');
+			self.setSubExp1(null);
 			for(var i =0; i < this.currentWires.length; i++)
 			{
 				var wire = this.currentWires[i];
@@ -102,12 +257,17 @@ define(function ()
 					wireSubSource = wire[3];
 					wireSubTarget = wire[4];
 					wireFunc = wire[5];
+					self.setSubExp1(wireSubSource);
+					self.setSubExp2(wireSubTarget);
 				}
 			}
 			if(wiredToID)
 			{
 				this.setSource(wiredToID)
 				this.setSourceProp(wiredToProp);
+				$("#wireeditordisable").show();
+				$(".wireeditorright").addClass('wiredisabled');
+				
 			}
 
 		}
@@ -115,6 +275,9 @@ define(function ()
 		{
 			$('#WireEditorSelectedProps').empty();
 			$('#WireEditorSourceProps').empty();
+			$('#WireEditorSelectedName').text(vwf.getProperty(this.selectedNodeID,'DisplayName') ||this.selectedNodeID) ;
+			$("#wireeditordisable").show();
+			$(".wireeditorright").addClass('wiredisabled');
 			var props = this.getProperties(this.selectedNodeID);
 			for(var i in props)
 			{
@@ -123,12 +286,14 @@ define(function ()
 
 				$('#targetprop'+i).click(function(e)
 				{
-					debugger;
+					
 					self.selectProp($(this).text());
 					$(".targetpropchoice").removeClass('wirepropselected');
 					$(this).addClass('wirepropselected');
 				})
 			}
+			this.updateTargetProps();
+			self.setSource(self.pickSourceID);
 		}
 	}
 });
