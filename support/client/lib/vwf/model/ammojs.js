2e0dcb1535b6363426f79107f8aaa54315d4f6e5
rchadwic <rchadwic@gmail.com>
2014-06-25 14:17:33 -0400

    basic load of driver and library

---
+++ support/client/lib/vwf/model/ammojs.js
@@ -0,0 +1,92 @@
+"use strict";
+
+// Copyright 2012 United States Government, as represented by the Secretary of Defense, Under
+// Secretary of Defense (Personnel & Readiness).
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
+
+/// vwf/model/object.js is a backstop property store.
+/// 
+/// @module vwf/model/object
+/// @requires vwf/model
+/// @requires vwf/configuration
+
+define(["module", "vwf/model", "vwf/configuration"], function(module, model, configuration) {
+
+    return model.load(module, {
+
+        // == Module Definition ====================================================================
+
+        // -- initialize ---------------------------------------------------------------------------
+
+        initialize: function() {
+            
+        },  
+
+        // == Model API ============================================================================
+
+        // -- creatingNode -------------------------------------------------------------------------
+
+        creatingNode: function(nodeID, childID, childExtendsID, childImplementsIDs, childSource, childType, childIndex, childName, callback /* ( ready ) */ ) {
+
+
+        },
+
+        // -- initializingNode ---------------------------------------------------------------------
+
+        initializingNode: function(nodeID, childID, childExtendsID, childImplementsIDs, childSource, childType, childIndex, childName) {
+
+        },
+
+        // -- deletingNode -------------------------------------------------------------------------
+
+        deletingNode: function(nodeID) {
+
+        },
+
+        // -- settingProperties --------------------------------------------------------------------
+
+        settingProperties: function(nodeID, properties) {},
+
+        // -- gettingProperties --------------------------------------------------------------------
+
+        gettingProperties: function(nodeID, properties) {
+
+        },
+
+        // -- creatingProperty ---------------------------------------------------------------------
+
+        creatingProperty: function(nodeID, propertyName, propertyValue) {
+            return this.initializingProperty(nodeID, propertyName, propertyValue);
+        },
+
+        // -- initializingProperty -----------------------------------------------------------------
+
+        initializingProperty: function(nodeID, propertyName, propertyValue) {
+            return this.settingProperty(nodeID, propertyName, propertyValue);
+        },
+
+        // TODO: deletingProperty
+
+        // -- settingProperty ----------------------------------------------------------------------
+
+        settingProperty: function(nodeID, propertyName, propertyValue) {
+
+        },
+
+        // -- gettingProperty ----------------------------------------------------------------------
+
+        gettingProperty: function(nodeID, propertyName, propertyValue) {
+
+        },
+    });
+
+});
