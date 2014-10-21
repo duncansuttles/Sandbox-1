5722b70fcd9d0d6663aa766f35c568479e309f00
Rob Chadwick <rchadwic@gmail.com>
2014-10-20 23:20:36 -0400

    integrate three post process pipeline, add first stab at IR rendering

---
+++ support/client/lib/vwf/model/threejs/ThermalCamEffect.js
@@ -0,0 +1,77 @@
+define(["module", "vwf/view", "vwf/view/threejs/postprocessing/EffectComposer",
+    "vwf/view/threejs/postprocessing/RenderPass",
+    "vwf/view/threejs/postprocessing/MaskPass",
+    "vwf/view/threejs/postprocessing/ShaderPass",
+    "vwf/view/threejs/postprocessing/BloomPass",
+    "vwf/view/threejs/postprocessing/FilmPass",
+    "vwf/view/threejs/shaders/CopyShader",
+    "vwf/view/threejs/shaders/FilmShader",
+    "vwf/view/threejs/shaders/ConvolutionShader",
+    "vwf/view/threejs/shaders/VignetteShader",
+
+
+], function() {
+
+    var THREE = window.THREE || {};
+    THREE.ThermalCamEffect = function(renderer, scene, camera) {
+        var composer = new THREE.EffectComposer(renderer);
+         var renderScene = new THREE.RenderPass(scene, camera);
+
+        composer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+        composer.addPass(renderScene);
+        var effectBloom = new THREE.BloomPass(1);
+        
+        composer.addPass(effectBloom);
+        var filma = new THREE.FilmPass(0.35, 0.5, 512, true);
+        var filmb = new THREE.FilmPass(0.35, 0.5, 2048, true);
+        var shaderVignette = THREE.VignetteShader;
+        var effectVignette = new THREE.ShaderPass(shaderVignette);
+        effectVignette.uniforms["offset"].value = 0.95;
+        effectVignette.uniforms["darkness"].value = 1.6;
+        composer.addPass(filma);
+        composer.addPass(filmb);
+        composer.addPass(effectVignette);
+        effectVignette.renderToScreen = true;
+        this.matBackup = {};
+        this.updateMats = function() {
+            for (var i in _MaterialCache.materials) {
+                if (this.matBackup[i]) continue;
+                var def = JSON.parse(i);
+                var mat = _MaterialCache.materials[i];
+                var rest = {};
+                rest.mat = mat;
+                this.matBackup[i] = rest;
+                mat.alphaMap = mat.map;
+                mat.map = null;
+                if (mat.diffuse) {
+                    mat.diffuse.r = 1;
+                    mat.diffuse.g = 1;
+                    mat.diffuse.b = 1;
+                }
+                if (mat.emissive) {
+                    mat.emissive.r = def.IR || .2;
+                    mat.emissive.g = def.IR || .2;
+                    mat.emissive.b = def.IR || .2;
+                }
+               
+                mat.needsUpdate = true;
+                
+            }
+        }
+        this.render = function(scene, camera) {
+
+        	
+            this.updateMats();
+            composer.render();
+            effectBloom.materialConvolution.uniforms.uImageIncrement.value.x = .001;
+       	    effectBloom.materialCopy.uniforms.opacity.value = 1.2;
+        }
+        this.setSize = function(x, y) {
+            composer.setSize(x / 2, y / 2);
+
+        }
+
+
+
+    }
+});
