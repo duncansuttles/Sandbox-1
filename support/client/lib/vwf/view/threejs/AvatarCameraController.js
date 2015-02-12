95e3a77e2ceb671028e9c63634d4b437e87e099b
rchadwic <rchadwic@gmail.com>
2015-02-12 17:42:32 -0500

    swing camera to follow turn in avatar view

--- support/client/lib/vwf/view/threejs/AvatarCameraController.js
+++ support/client/lib/vwf/view/threejs/AvatarCameraController.js
@@ -21,25 +21,27 @@ var AvatarCameraController = function()
         this.mouseDown = false;
         this.shiftDown = false;
         this.ctrlDown = false;
+        this.keysDown = {};
     }
     this.localTouchStart = function(event) {}
     this.localTouchEnd = function(event) {}
     this.localTouchMove = function(event) {}
     this.localpointerDown = function(e, pickInfo)
     {
-        if(e.button == 2)
+        if (e.button == 2)
             this.mouseDown = true;
         this.last_x = e.clientX;
         this.last_y = e.clientY;
     }
     this.localpointerUp = function(e, pickInfo)
     {
-        if(e.button == 2)
+        if (e.button == 2)
             this.mouseDown = false;
     }
     this.localKeyDown = function(e)
     {
-        console.log(e.keyCode)
+       // if(e.keyCode !=87 && e.keyCode != 38 && this.keysDown[e.keyCode]) return;
+        this.keysDown[e.keyCode] = true;
         var id = _UserManager.GetAvatarForClientID(vwf.moniker()).id;
         if (e.keyCode == 16)
             this.shiftDown = true;
@@ -74,35 +76,43 @@ var AvatarCameraController = function()
             ]);
         }
     }
-    this.localKeyUp = function(e) {
+    this.localKeyUp = function(e)
+    {
+        delete this.keysDown[e.keyCode];
         if (e.keyCode == 16)
             this.shiftDown = false;
-         if (e.keyCode == 17)
+        if (e.keyCode == 17)
             this.ctrlDown = false;
     }
     this.localpointerMove = function(e, pickInfo)
     {
-        
-         this.rel_x = e.clientX - this.last_x;
-      this.last_x = e.clientX;
-        if (this.mouseDown || this.shiftDown ){
-               
-                var rot_z = new THREE.Quaternion();
-                rot_z.setFromAxisAngle(new THREE.Vector3(0, 0, 1), -this.rel_x / 300);
-                this.offset = this.offset.applyQuaternion(rot_z);}
-        
+        this.rel_x = e.clientX - this.last_x;
+        this.last_x = e.clientX;
+        if (this.mouseDown || this.shiftDown)
+        {
+            var rot_z = new THREE.Quaternion();
+            rot_z.setFromAxisAngle(new THREE.Vector3(0, 0, 1), -this.rel_x / 300);
+            this.offset = this.offset.applyQuaternion(rot_z);
+        }
         this.rel_y = e.clientY - this.last_y;
         this.last_y = e.clientY;
-        if (this.mouseDown  || this.shiftDown)
+        if (this.mouseDown || this.shiftDown)
             this.totalz += this.rel_y;
-        else if(this.ctrlDown)
+        else if (this.ctrlDown)
         {
-            this.zoom += this.rel_y/100;
+            this.zoom += this.rel_y / 100;
         }
     }
     this.orientationEvent = function(e) {}
     this.updateCamera = function()
     {
+        if (Object.keys(this.keysDown).length > 0)
+        {
+            var transform = vwf.getProperty(_UserManager.GetAvatarForClientID(vwf.moniker()).id, 'transform');
+            var charspaceforward = Mat4.multVec3NoTranslate(transform, [0, 1, 0], []);
+            this.offset.x = this.offset.x * .97 + charspaceforward[0] * .03;
+            this.offset.y = this.offset.y * .97 + charspaceforward[1] * .03;
+        }
         var avatar = _UserManager.GetAvatarForClientID(vwf.moniker()).transformAPI.getPosition();
         var center = new THREE.Vector3(avatar[0], avatar[1], avatar[2] + 1.5);
         var pos = center.clone().add(this.offset.setLength(this.zoom));
@@ -118,8 +128,7 @@ var AvatarCameraController = function()
             this.zoom *= 1.1;
         else
             this.zoom *= .9;
-
-        if(this.zoom < .5)
+        if (this.zoom < .5)
             require("vwf/view/threejs/editorCameraController").setCameraMode('FirstPerson');
     }
     this.prerender = function()
